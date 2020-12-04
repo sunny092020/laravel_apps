6b3282f26684d9bc384556b2fe6032ffe0c6de59
Vu Hoang Giang <itworkjp@gmail.com>
2020-12-05 00:02:20 +0900

    add order page

--- oms/app/Http/Controllers/OrderController.php
+++ oms/app/Http/Controllers/OrderController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class OrderController extends Controller
 {
     //
+    public function new()
+    {
+        return view('order.new', ['products' => Product::all()]);
+    }
+
 }
--- oms/routes/web.php
+++ oms/routes/web.php
@@ -12,7 +12,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+use App\Http\Controllers\OrderController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/orders/new', [OrderController::class, 'new']);
