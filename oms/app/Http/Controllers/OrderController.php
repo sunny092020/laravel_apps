0853c2bfe2f9950199facbeb41c289f97d4c992d
Vu Hoang Giang <itworkjp@gmail.com>
2020-12-05 00:30:45 +0900

    add order page

--- oms/app/Http/Controllers/OrderController.php
+++ oms/app/Http/Controllers/OrderController.php
@@ -10,6 +10,10 @@ class OrderController extends Controller
     //
     public function new()
     {
+        foreach (Product::all() as $product) {
+            echo $product->name;
+        }
+        
         return view('order.new', ['products' => Product::all()]);
     }
 
