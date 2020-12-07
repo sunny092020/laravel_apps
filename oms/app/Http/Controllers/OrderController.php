6501b5aad325505144373f15e35bf70f55bc495a
Vu Hoang Giang <itworkjp@gmail.com>
2020-12-07 17:51:58 +0900

    add transaction

--- oms/app/Http/Controllers/OrderController.php
+++ oms/app/Http/Controllers/OrderController.php
@@ -10,6 +10,7 @@ use App\Models\OrderDetail;
 use Carbon\Carbon;
 
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\DB;
 
 class OrderController extends Controller
 {
@@ -29,32 +30,32 @@ class OrderController extends Controller
         $quantity = $request->input('quantity');
         $user_id = 1;
 
-        $now = Carbon::now();
-        $start_of_day = $now->copy()->startOfDay();
-        $end_of_day = $now->copy()->endOfDay();
+        DB::transaction(function() use($department_id, $product_id, $quantity, $user_id) {
+            $now = Carbon::now();
+            $start_of_day = $now->copy()->startOfDay();
+            $end_of_day = $now->copy()->endOfDay();
+            $today_order = Order::
+                where('ordered_at', '>=', $start_of_day)->
+                where('ordered_at', '<=', $end_of_day)->first();
 
-        $today_order = Order::where('ordered_at', '>=', $start_of_day)->where('ordered_at', '<=', $end_of_day)->first();
-        Log::debug($today_order);
-        Log::debug($start_of_day);
-        Log::debug($end_of_day);
-        
-        if(!$today_order) {
-            Log::debug("here create new");
-
-            $order = Order::create([
-                'ordered_at' => $now,
-                'ordered_from_department_id' => $department_id
-            ]);
-            $order_id = $order->save();
-        } else {
-            $order_id = $today_order->id;
-        }
-
-        OrderDetail::updateOrCreate(
-            ['order_id' => $order_id, 'product_id' => $product_id],
-            ['quantity' => $quantity, 'ordered_user_id' => $user_id]
-        );
-        
+            if(!$today_order) {
+                Log::debug("here create new");
+    
+                $order = Order::create([
+                    'ordered_at' => $now,
+                    'ordered_from_department_id' => $department_id
+                ]);
+                $order_id = $order->save();
+            } else {
+                $order_id = $today_order->id;
+            }
+    
+            OrderDetail::updateOrCreate(
+                ['order_id' => $order_id, 'product_id' => $product_id],
+                ['quantity' => $quantity, 'ordered_user_id' => $user_id]
+            );    
+        });
+                
         return response()->json(['success'=>'Ajax request submitted successfully']);
     }
 
